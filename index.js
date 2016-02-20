9d50379fc0c342c3b55f33041df81beb174d3ecb
Heinrich <heinrich@aftership.net>
2016-02-20 12:40:02 +0800

    version 3

--- index.js
+++ index.js
@@ -2,14 +2,15 @@
 
 const scraper = require('./lib/scraper');
 const db = require('./lib/db');
-
-const DB = new db();
+const Redis = require('ioredis');
+const redis = new Redis();
+const DB = new db(redis);
 
 scraper(function (err, temp) {
 	console.log('this is from the scraper', temp);
 	DB.set('temp', temp);
 
-	DB.get('temp', function (result) {
+	DB.get('temp', function (error, result) {
 		console.log('this is from the db', result);
 	});
 });
